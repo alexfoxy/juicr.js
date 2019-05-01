88f21ee21116c84c13de6cb8bda702f1139cd9e1
Alex Fox <alexfoxy@gmail.com>
2019-05-01 13:11:22 +0100

    added _codenpen folder

---
+++ _codepen/todoJuicr.js
@@ -0,0 +1,97 @@
+window.createToDoJuicer = (state) => {
+  const juicer = new window.Juicer({ initialState: state, dev: true })
+
+  let id = 0
+
+  function findToDoFromId(todos, id) {
+    const i = todos.findIndex(t => t.id = id)
+    return todos[i]
+  }
+
+  juicer.action('addTodo', ({ }, _state) => {
+    if(_state.newTodo.length === 0) {
+      return { errorText: "cannot create empty todo" }
+    } else {
+      id++
+
+      const todo = {
+        title: _state.newTodo,
+        isComplete: false,
+        isEditing: false,
+        id
+      }
+
+      return { newTodo: '', todos: [todo, ..._state.todos] }
+    }
+  })
+
+  juicer.action('textInput', ({ text }, _state) => {
+    return { newTodo: text }
+  })
+
+  juicer.action('setFilter', ({ filter }, _state) => {
+    return { filter }
+  })
+
+  juicer.action('toggleAll', ({ }, _state) => {
+    return {
+      todos: _state.todos.map((t) => {
+        t.isComplete = true
+        return t
+      })
+    }
+  })
+
+  juicer.action('toggleDone', ({ id }, _state) => {
+    const todo = findToDoFromId(_state.todos, id)
+    if(todo) {
+      todo.isComplete = !todo.isComplete
+      return { todos: _state.todos }
+    } else {
+      throw Error(`could not find todo with id ${id}`)
+    }
+  })
+
+  juicer.action('toggleEdit', ({ id }, _state) => {
+    const todo = findToDoFromId(_state.todos, id)
+    if(todo) {
+      todo.isEditing = !todo.isEditing
+      return { todos: _state.todos, isEditing: todo.isEditing }
+    } else {
+      throw Error(`could not find todo with id ${id}`)
+    }
+  })
+
+  juicer.action('updateTodo', ({ id, title }, _state) => {
+    const todo = findToDoFromId(_state.todos, id)
+    if(todo) {
+      todo.title = title
+      return { todos: _state.todos }
+    } else {
+      throw Error(`could not find todo with id ${id}`)
+    }
+  })
+
+  juicer.action('clearCompleted', ({}, _state) => {
+    return { todos: _state.todos.filter(t => t.isComplete === false ) }
+  })
+
+  juicer.action('delete', ({ id }, _state) => {
+    return { todos: _state.todos.filter(t => t.id !== id ) }
+  })
+
+  juicer.reaction(["todos", "filter"], (changedState, _state) => {
+    const filters = {
+      all: (t) => { return t },
+      completed: (t) => { return t.isComplete === true },
+      active: (t) => { return t.isComplete === false }
+    }
+
+    return {
+      filteredTodos: _state.todos.filter(filters[_state.filter]),
+      remainingTodos: _state.todos.filter(filters['active']).length
+    }
+  })
+
+  return juicer
+}
